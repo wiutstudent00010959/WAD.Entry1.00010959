294f9be4d16c193176aa1a29f002f3eb64e8e19a
wiutstudent_00010959 <91773131+wiutstudent00010959@users.noreply.github.com>
2023-04-04 15:38:04 +0500

    hme page updated

--- WAD.CW.10959/Models/Item.cs
+++ WAD.CW.10959/Models/Item.cs
@@ -7,6 +7,7 @@ namespace WAD.CW._10959.Models
 {
     public class Item
     {
+        //basic prperties
         public int Id { get; set; }
         public string Title { get; set; }
         public int Quantity { get; set; }
