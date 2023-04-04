dc9b31df261f53b920e743fc48640fc03513e0f6
wiutstudent_00010959 <91773131+wiutstudent00010959@users.noreply.github.com>
2023-04-04 15:29:38 +0500

    item class created

---
+++ WAD.CW.10959/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WAD.CW._10959.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WAD.CW.10959/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WAD.CW._10959.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Quantity { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
