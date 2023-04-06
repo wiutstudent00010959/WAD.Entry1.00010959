754494fb6610532a4011b61451e5c86f90575c9d
wiutstudent_00010959 <91773131+wiutstudent00010959@users.noreply.github.com>
2023-04-06 23:20:21 +0500

    --

---
+++ WAD.CW.00010959/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WAD.CW._00010959.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
