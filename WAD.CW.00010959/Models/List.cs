3d79ff23b41809cd855f47676fad552e0ca1c729
wiutstudent_00010959 <91773131+wiutstudent00010959@users.noreply.github.com>
2023-04-08 00:35:28 +0500

    technically cprrect app restprred + zip archive add

---
+++ WAD.CW.00010959/Models/List.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WAD.CW._00010959.Models
+{
+    public class List
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+        public DateTime CreatedDate { get; set; }
+
+        public ICollection<Item> Items { get; set; }
+    }
+}
---
+++ WAD.CW.00010959/Models/ListItemsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WAD.CW._00010959.Models
+{
+    public class ListItemsViewModel
+    {
+        public int ListId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        // public List<Item> Items { get; set; }
+        public List<Item> Items { get; set; }
+        public List<List> Lists { get; set; }
+    }
+}
