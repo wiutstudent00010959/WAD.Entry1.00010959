61debd1be0560a0517eabb6901a943251854b119
wiutstudent_00010959 <91773131+wiutstudent00010959@users.noreply.github.com>
2023-04-07 13:04:49 +0500

    data mdel crud

---
+++ WAD.CW.00010959/Migrations/20230407075732_ItemMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WAD.CW._00010959.Migrations
+{
+    public partial class ItemMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(maxLength: 50, nullable: false),
+                    Description = table.Column<string>(maxLength: 500, nullable: true),
+                    Quantity = table.Column<int>(nullable: false, defaultValue: 0),
+                    IsPurchased = table.Column<bool>(nullable: false, defaultValue: false),
+                    PurchaseDate = table.Column<DateTime>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
--- WAD.CW.00010959/Startup.cs
+++ WAD.CW.00010959/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -7,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WAD.CW._00010959.Models;
 
 namespace WAD.CW._00010959
 {
@@ -23,6 +25,8 @@ namespace WAD.CW._00010959
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ItemContext>(item => item.UseSqlServer(Configuration.GetConnectionString("MyDbContext")));
+           // services.AddSingleton(new MySettings(someSetting, anotherSetting, yetAnotherSetting));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
