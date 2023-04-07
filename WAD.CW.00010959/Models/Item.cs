a75340b7a9dbbe029d3bea315838f9f47c9f87cd
wiutstudent_00010959 <91773131+wiutstudent00010959@users.noreply.github.com>
2023-04-07 11:51:11 +0500

    data mdel develpment

---
+++ WAD.CW.00010959/Models/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WAD.CW._00010959.Models
+{
+    public class Item
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Range(1, 100, ErrorMessage = "Quantity must be between 1 and 100.")]
+        public int Quantity { get; set; }
+
+        public bool IsPurchased { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Purchase Date")]
+        public DateTime PurchaseDate { get; set; }
+    }
+}
---
+++ WAD.CW.00010959/Models/ItemContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WAD.CW._00010959.Models
+{
+    public class ItemContext : DbContext
+    {
+
+        public ItemContext(DbContextOptions<ItemContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>()
+                .Property(i => i.Title)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Item>()
+                .Property(i => i.Quantity)
+                .IsRequired()
+                .HasDefaultValue(0);
+
+            modelBuilder.Entity<Item>()
+                .Property(i => i.IsPurchased)
+                .IsRequired()
+                .HasDefaultValue(false);
+
+            modelBuilder.Entity<Item>()
+                .Property(i => i.Description)
+                .HasMaxLength(500);
+
+            modelBuilder.Entity<Item>()
+                .Property(i => i.PurchaseDate)
+                .IsRequired()
+                .HasColumnType("date");
+        }
+
+    }
+}
